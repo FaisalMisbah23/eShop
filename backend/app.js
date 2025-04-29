1c21508d16f58e050a70d02b8abb90f88592cdf7
Faisal Misbah <faisalmisbah23@gmail.com>
2025-04-29 16:27:21 +0500

    Update frontend url

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://eshopzone.vercel.app/",
+    origin: "https://eshopzone.vercel.app",
     credentials: true,
   })
 );
